8d9be3f2897fa15ca9268d68c5eea2a924c364bd
primedev88 <119492906+primedev88@users.noreply.github.com>
2023-09-23 07:37:03 +0000

    learned

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score ="33abc"
+let score2=null
+console.log(typeof score);
+console.log(typeof score2);
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+let value2 = Number(score2)
+console.log(typeof value2);
+console.log(value2);
+
+
+// 1=> true; 0 => false
+// "" => false
+// "asdf" => true
+
+// ******************************* Operations ******************************************
+let str1="hello"
+let str2="anuj"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log("1"+2+2)
+
+console.log(true);
+console.log(+true);
+console.log(+"");
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive
+//7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100 //(jvascript is dynamically typed)
+
+
+//Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros=["abc","cdf","sdfa"];
+
+let myObj = {
+    name:"Anuj",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
