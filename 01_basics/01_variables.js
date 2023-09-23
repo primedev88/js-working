1d4c67fb7abd19e1f173d9243f91f68582d84fb5
primedev88 <119492906+primedev88@users.noreply.github.com>
2023-09-23 06:21:27 +0000

    learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const account_Id = 144554
+let account_Email= "anujk7@gmail.com"
+var account_Password= "12983"
+account_City = "Jaipur"
+
+// Prefer not to use var beacuse of problem with scope
+let accountState;
+console.table([account_Id,account_Email,account_Password,account_City,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS as newer version
+
+// alert("hello")   This only works in browser not in node
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value and is an object
+undefined => value not assigned
+symbol => unique
+*/
+
+console.log(typeof "anuj")
