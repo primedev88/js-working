2c699efa54725da081a1fe06618ff598ed8506c5
primedev88 <119492906+primedev88@users.noreply.github.com>
2023-09-29 06:08:15 +0000

    learned

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -10,27 +10,42 @@ console.log(Object.keys(tinderUser))
 console.log(Object.values(tinderUser))
 console.log(Object.entries(tinderUser))
 
-// const regularUser={
-//     email:"sjdfgijdf",
-//     fullname:{
-//         userfullname:{
-//             firstname:"Anuj",
-//             lastname:"kumar"
-//         }
-//     }
-// }
-// console.log(regularUser.fullname)
-// console.log(regularUser.fullname.userfullname)
-// console.log(regularUser.fullname.userfullname.firstname)
-// console.log(regularUser.fullname?.userfullname.firstname) //checks if name doesn't exist
+const regularUser={
+    email:"sjdfgijdf",
+    fullname:{
+        userfullname:{
+            firstname:"Anuj",
+            lastname:"kumar"
+        }
+    }
+}
+console.log(regularUser.fullname)
+console.log(regularUser.fullname.userfullname)
+console.log(regularUser.fullname.userfullname.firstname)
+console.log(regularUser.fullname?.userfullname.firstname) //checks if name doesn't exist
 
-// const obj1={1:"a",2:"b"}
-// const obj2={3:"c",4:"d"}
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
 
-// const obj3={obj1,obj2}
-// const obj4=Object.assign(obj1,obj2)
-// console.log(obj3)
-// console.log(obj4)
+const obj3={obj1,obj2}
+const obj4=Object.assign(obj1,obj2)
+console.log(obj3)
+console.log(obj4)
 
-// const obj5={...obj1,...obj2}
-// console.log(obj5)
+const obj5={...obj1,...obj2}
+console.log(obj5)
+
+const course={
+    coursename:"js in english",
+    price:"999",
+    courseInstructor:"Anuj"
+}
+
+const {courseInstructor:instructor}=course
+//console.log(courseInstructor)
+console.log(instructor)
+
+const navbar = ({company})=>{
+    console.log(company)
+}
+navbar({company:"Anuj"})
